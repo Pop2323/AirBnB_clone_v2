637e5d4e517e0e7a808d503cff313f70e121e82d
Pop2323 <omar4500455@gmail.com>
2024-01-30 10:44:21 -0800

    web_flask_update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -13,43 +13,41 @@ The default value of text is “is cool”
 You must use the option strict_slashes=False in your route definition
 """
 
-from email.policy import strict
 from flask import Flask
 
-app = False(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """display “Hello HBNB!”"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """display “HBNB”"""
-    return ("HBNB")
+    return "HBNB"
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def C(text):
     """display “C ” followed by the value of the text variable"""
-    return ("C {}".format(text.replace("_", " ")))
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """display “Python ” followed by the value of the text variable"""
-    return ("Python {}".format(text.replace("_", " ")))
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return ("{} is a number".format(n))
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000, debug=True)
