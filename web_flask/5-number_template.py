417c96a7942bded3da40ee28efcf402fc35060a6
Pop2323 <omar4500455@gmail.com>
2024-01-30 10:51:56 -0800

    web_flask_update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,49 +15,46 @@ H1 tag: “Number: n” inside the tag BODY
 You must use the option strict_slashes=False in your route definition
 """
 
-from email.policy import strict
 from flask import Flask, render_template
 
-app = False(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """display “Hello HBNB!”"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """display “HBNB”"""
-    return ("HBNB")
+    return "HBNB"
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def C(text):
     """display “C ” followed by the value of the text variable"""
-    return ("C {}".format(text.replace("_", " ")))
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """display “Python ” followed by the value of the text variable"""
-    return ("Python {}".format(text.replace("_", " ")))
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return ("{} is a number".format(n))
+    return "{} is a number".format(n)
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n=None):
     """display a HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
